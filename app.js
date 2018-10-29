5124b43e8bf0cf88792011124898cf39d0c2ba20
rkgallaway <rkgallaway@msn.com>
2018-10-29 15:22:22 -0700

    app.js garden cunstructor done

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
 'use stirct';
 
-//when "get started" is clicked, begin survey
-    //event listener -need to know when its clicked a link to href
+var userGarden = [];
+
+function Garden(name, gardenType, lunchPreference, experienceLevel){
+  this.name = name;
+  this.gardenType = gardenType;
+  this.lunchPreference = lunchPreference;
+  this.experienceLevel = experienceLevel;
+  userGarden.push(this);
+}
+//example:  cunstructor working
+// new Garden('Ryan', 'sun', 'sandwich', 'beginner');
+
+//create instance:: working ahead
+//starting on eventlistener below
+
+// function letsGetGrowing(event){
+// 	event.preventDefaults();
+// 	var name = event.target.name.value;
+// 	var gardenType = event.target.
+// }
+
+// //cookie-stand examples:
+// function handleStoreSubmit(event) {
+//   event.preventDefault();
+//   var storeLocale = event.target.storelocation.value;
+//   var minCust = Number(event.target.mincustomers.value);
+//   var maxCust = Number(event.target.maxcustomers.value);
+//   var avgCookies = event.target.avgcookies.value;
+
+//   var newLocation = new Store(storeLocale, minCust, maxCust, avgCookies);
+
+//   event.target.storelocation.value = null;
+//   event.target.mincustomers.value = null;
+//   event.target.maxcustomers.value = null;
+//   event.target.avgcookies.value = null;
+
+//   storeTable.innerHTML='';
+//   tableHeader();
+
+//   newLocation.custCount();
+//   newLocation.cookieCount();
+//   newLocation.sumOfCookies();
+//   totalOfTotals = 0;  //added this also
+
+//   callRender();
+//   tableFooter();
+// }
+
+
+
+//when "get started" is clicked, begin survey --stretch
+//event listener -need to know when its clicked a link to href stretch
 
 //collect name 
+
 
 // collect garden type sun / shade / partial sun
 
