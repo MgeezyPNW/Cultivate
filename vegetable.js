8e90293202bbf8a879b416c49aa58303ff85b85d
Michael George <mgeorge9492@gmail.com>
2018-10-29 16:59:54 -0700

    list renders

--- vegetable.js
+++ vegetable.js
@@ -3,6 +3,7 @@
 // Global Variables 
 var allVegetables = [];
 var allPlants = [['tomato', 'mixed', 'big'], ['cucumber', 'sun', 'big'], ['lettuce', 'sun', 'small'], ['pepper', 'sun', 'small'], ['onions', 'sun', 'small'], ['herbs', 'mixed', 'small'], ['peas', 'shade', 'big'], ['beets', 'shade', 'small'], ['celery', 'mixed', 'small'], ['cherry tomatos', 'sun', 'big']];
+var tableList = document.getElementById('list');
 
 function Vegetable(name, sunPreference, vegetableSize) {
   this.name = name;
@@ -14,3 +15,12 @@ function Vegetable(name, sunPreference, vegetableSize) {
 for (var i = 0; i < allPlants.length; i++) {
   new Vegetable(allPlants[i][0], allPlants[i][1], allPlants[i][2]); 
 }
+
+for (var j = 0; j < allVegetables.length; j++) {
+  var liEl = document.createElement('li');
+  liEl.textContent = allVegetables[j].name;
+  tableList.appendChild(liEl);
+}
+
+
+
