025e9ae707a805781200013a61c2371aeba29146
Adam Wittnam <adam.wittnam@hotmail.com>
2018-10-30 13:12:15 -0700

    checking local storage

--- vegetable.js
+++ vegetable.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Global Variables 
+// Global Variables
 var allPlants = [['Heirloom/Beefsteak Tomatoes', 'Sun', 'Big'], ['Cucumbers', 'Sun', 'Big'], ['Red Leaf Lettuce', 'Sun', 'Small'], ['Sweet Bell Peppers', 'Sun', 'Small'], ['Onions', 'Sun', 'Small'], ['Basil', 'Sun', 'Small'], ['Early Girl/Cherry Tomatoes', 'Mixed', 'Big'], ['Snow Peas', 'Mixed', 'Big'], ['Green Leaf Lettuce', 'Mixed', 'Small'], ['Italian Basil', 'Mixed', 'Small'], ['Celery', 'Mixed', 'Small'],['Banana Peppers', 'Mixed', 'Small'], ['Cherry Tomatoes', 'Shade', 'Big'],['Snow/Snap Peas', 'Shade', 'Big'], ['Beets', 'Shade', 'Small'], ['Parisian Carrots', 'Shade', 'Small'], ['Spring Mix Lettuce', 'Shade', 'Small'], ['Chives', 'Shade', 'Small']];
 var allVegetables = [];
 var bigSunPlants = [];
@@ -21,6 +21,16 @@ var front_right_center = document.getElementById('front_right_center');
 var front_right = document.getElementById('front_right');
 var tableList = document.getElementById('list');
 
+function checkLocalStorage() {
+  if (localStorage.garden) {
+  //create instances
+    var retrievedGarden = JSON.parse(localStorage.getItem('garden'));
+    console.log('retrievedGarden', retrievedGarden);
+    retrievedGarden.forEach(function(gardenItem){
+      new Vegetable(gardenItem.name, gardenItem.sunPreference, gardenItem.vegetableSize);
+    })
+  }
+}
 
 function Vegetable(name, sunPreference, vegetableSize) {
   this.name = name;
@@ -29,10 +39,10 @@ function Vegetable(name, sunPreference, vegetableSize) {
   allVegetables.push(this);
   if (this.vegetableSize === 'Big' && this.sunPreference === 'Sun') {
     bigSunPlants.push(this);
-  } 
+  }
   if (this.vegetableSize === 'Big' && this.sunPreference === 'Mixed') {
     bigMixedPlants.push(this);
-  } 
+  }
   if (this.vegetableSize === 'Big' && this.sunPreference === 'Shade') {
     bigShadePlants.push(this);
   }
@@ -47,15 +57,17 @@ function Vegetable(name, sunPreference, vegetableSize) {
   }
 }
 
-for (var i = 0; i < allPlants.length; i++) {
-  new Vegetable(allPlants[i][0], allPlants[i][1], allPlants[i][2]); 
-}
 
-for (var j = 0; j < allVegetables.length; j++) {
-  var liEl = document.createElement('li');
-  liEl.textContent = allVegetables[j].name;
-  tableList.appendChild(liEl);
-}
+
+// for (var i = 0; i < allPlants.length; i++) {
+//   new Vegetable(allPlants[i][0], allPlants[i][1], allPlants[i][2]);
+// }
+
+// for (var j = 0; j < allVegetables.length; j++) {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = allVegetables[j].name;
+//   tableList.appendChild(liEl);
+// }
 
 
 
