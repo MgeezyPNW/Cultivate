325c7d2a2778e71aad29fa623c4ea097420b7d98
Heather Cherewaty <hr.cherewaty@gmail.com>
2018-10-30 11:00:58 -0700

    Updates results.html to include classes for css grid.  Updates vegetables.js to include sun, shade, mixed veg list and variables to access dom

--- vegetable.js
+++ vegetable.js
@@ -2,8 +2,21 @@
 
 // Global Variables 
 var allVegetables = [];
-var allPlants = [['tomato', 'mixed', 'big'], ['cucumber', 'sun', 'big'], ['lettuce', 'sun', 'small'], ['pepper', 'sun', 'small'], ['onions', 'sun', 'small'], ['herbs', 'mixed', 'small'], ['peas', 'shade', 'big'], ['beets', 'shade', 'small'], ['celery', 'mixed', 'small'], ['cherry tomatos', 'sun', 'big']];
+var allPlants = [['Heirloom/Beefsteak Tomatoes', 'Sun', 'Big'], ['Cucumbers', 'Sun', 'Big'], ['Red Leaf Lettuce', 'Sun', 'Small'], ['Sweet Bell Peppers', 'Sun', 'Small'], ['Onions', 'Sun', 'Small'], ['Basil', 'Sun', 'Small'], ['Early Girl/Cherry Tomatoes', 'Mixed', 'Big'], ['Snow Peas', 'Mixed', 'Big'], ['Green Leaf Lettuce', 'Mixed', 'Small'], ['Italian Basil', 'Mixed', 'Small'], ['Celery', 'Mixed', 'Small'],['Banana Peppers', 'Mixed', 'Small'], ['Cherry Tomatoes', 'Shade', 'Big'],['Snow/Snap Peas', 'Shade', 'Big'], ['Beets', 'Shade', 'Small'], ['Parisian Carrots', 'Shade', 'Small'], ['Spring Mix Lettuce', 'Shade', 'Small'], ['Chives', 'Shade', 'Small']];
+
+
+
+//Access DOM
+var largeContainer = document.getElementById('container_large');
+var back_left = document.getElementById('back_left');
+var back_right = document.getElementById('back_right');
+var smallContainer = document.getElementById('container_small');
+var front_left = document.getElementById('front_left');
+var front_left_center = document.getElementById('front_left_center');
+var front_right_center = document.getElementById('front_right_center');
+var front_right = document.getElementById('front_right');
 var tableList = document.getElementById('list');
+
 
 function Vegetable(name, sunPreference, vegetableSize) {
   this.name = name;
